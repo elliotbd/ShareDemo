b775889a3f8fe423121e7f6420c24c70b382fe9d
Brian Elliott <brian.elliott@fotf.org>
2015-08-11 15:01:45 -0600

    Update

---
+++ Debate Demo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Debate Demo
+//
+//  Created by Elliott, Brian on 8/11/15.
+//  Copyright © 2015 Focus on the Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AAShareBubbles.h"
+
+@interface ViewController : UIViewController <AAShareBubblesDelegate>
+
+
+@end
+
---
+++ Debate Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Debate Demo
+//
+//  Created by Elliott, Brian on 8/11/15.
+//  Copyright © 2015 Focus on the Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
